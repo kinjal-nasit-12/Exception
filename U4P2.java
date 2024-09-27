3735913a70b588488aca713665bafa0ab5fceb3e
kinjal-nasit-12 <kinjalnasit12@gmail.com>
2024-09-27 05:09:48 -0700

    Update U4P2.java

--- U4P2.java
+++ U4P2.java
@@ -1,14 +1,23 @@
+/* Write an application that accepts marks of three different subjects from user.
+Marks should be between 0 to 100, if marks of any of the subject do not belong to
+this range, generate custom exception out of RangeException. If marks of each
+subjects are greater than or equal to 40 then display message “PASS” along with
+percentage, otherwise display message “FAIL”. Also write exception handling code
+to catch all the possible runtime exceptions likely to be generated in the program.*/ 
 import java.util.Scanner;
+
 class RangeException extends Exception {
+
     public RangeException(String message) {
         super(message);
     }
 }
 
 public class U4P2 {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int totalMarks=0;
+        int totalMarks = 0;
         try {
             System.out.printf("How many subjects do you want to enter marks for? ", totalMarks);
             int numberOfSubjects = getIntegerValue(scanner);
