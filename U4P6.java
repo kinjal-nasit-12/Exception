75b98c4e68ab42150a1c8796e36ce8e823ed2781
kinjal-nasit-12 <kinjalnasit12@gmail.com>
2024-11-22 09:45:31 +0530

    Update U4P6.java

--- U4P6.java
+++ U4P6.java
@@ -32,21 +32,30 @@ class EvenOdd extends Thread {
 public class U4P6 {
 
     public static void main(String[] args) {
-        if (args.length != 2) {
-            System.out.println("Please provide exactly 2 numbers by command line.");
-            return;
-        }
+        int lb = 0;
+        int ub = 0;
 
-        int lb = Integer.parseInt(args[0]);
-        int ub = Integer.parseInt(args[1]);
-        if (lb > ub) {
-            System.out.println("Invalid range: Lower bound is greater than upper bound.");
+        try {
+
+            if (args.length != 2) {
+                System.out.println("Please provide exactly 2 integer numbers by command line.");
+                return;
+            }
+
+            lb = Integer.parseInt(args[0]);
+            ub = Integer.parseInt(args[1]);
+
+            if (lb > ub) {
+                System.out.println("Invalid range>> Lower bound is greater than upper bound.");
+                return;
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Enter integer value. Exception>> " + e.getMessage());
             return;
         }
 
         EvenOdd even = new EvenOdd(lb, ub, true);
         EvenOdd odd = new EvenOdd(lb, ub, false);
-
         even.start();
         odd.start();
 
@@ -57,6 +66,6 @@ public class U4P6 {
             System.out.println("Main thread interrupted: " + e.getMessage());
         }
 
-        System.out.println("Completed task.");
+        System.out.println("Both threads completed.");
     }
 }
